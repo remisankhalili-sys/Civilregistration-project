116ed903d81fc7777de513befc1ea7fb66a0c852
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-09 00:55:38 +0330

    Civil Registry Information

--- users/models.py
+++ users/models.py
@@ -9,3 +9,12 @@ class UserProfile(models.Model):
     Contains civil registry information and consumption management settings.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    # Civil Registry Information
+    national_code = models.CharField(max_length=10, unique=True, verbose_name="National Code")
+    first_name = models.CharField(max_length=50, verbose_name="First Name")
+    last_name = models.CharField(max_length=50, verbose_name="Last Name")
+    phone_number = models.CharField(max_length=15, unique=True, verbose_name="Phone Number")
+    birth_date = models.DateField(null=True, blank=True, verbose_name="Date of Birth")
+    address = models.TextField(blank=True, null=True, verbose_name="Address")
+    
