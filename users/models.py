30e1496657e3790d4fcd84a44eab90111e1b6d2d
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-08 22:57:18 +0330

    Add in models: import and class userprofile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    Extended profile model for registered users.
+    Contains civil registry information and consumption management settings.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
