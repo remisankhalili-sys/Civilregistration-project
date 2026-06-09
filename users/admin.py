98592bb827f013b288431b1baee099c7b9be45c8
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-09 13:31:11 +0330

    Add in admin.py : imports and Class userprofile and customuseradmin.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile, SearchLog, AdminConsumptionLimit
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Civil Registry Info'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'profile__national_code', 'profile__phone_number')
+
+
