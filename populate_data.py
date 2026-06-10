c17202655cca7647da8bbbe89479c4b0fd40452d
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-10 12:28:39 +0330

    path settings for accessing the project.

--- populate_data.py
+++ populate_data.py
@@ -2,3 +2,8 @@ import os
 import sys
 import random
 import django
+
+# Path settings for accessing the project
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'civil_registry.settings')
+
