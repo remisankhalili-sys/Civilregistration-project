f6e5a32c6c3443c4baba949e211afd2932a510ce
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-10 12:30:00 +0330

    Setting up Django

--- populate_data.py
+++ populate_data.py
@@ -7,3 +7,11 @@ import django
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'civil_registry.settings')
 
+# Setting up Django
+django.setup()
+
+from django.contrib.auth.models import User
+from users.models import UserProfile, AdminConsumptionLimit
+from datetime import date
+
+def generate_national_code():
